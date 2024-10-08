7515e4ece6e9666bf2a352bbc46a21376bfc985d
MozartCuidadorDigital <45375029+nandostiwar@users.noreply.github.com>
2024-10-07 19:33:15 -0500

    se hizo copia del back

---
+++ controllers/calculadoraControllers.js
@@ -0,0 +1,47 @@
+const {add, subtract, multiply, numMax} = require('../operaciones/operaciones.js');
+
+function sumar(req, res){
+    const {body} = req;
+    const {number1, number2} = body;
+    const result = add(number1, number2);
+    console.log ("SUMA - Resultado: "+result);
+    res.json({
+        resultado: result
+    });
+}
+
+function restar(req, res){
+    const {body} = req;
+    const {number1, number2} = body;
+    const result = subtract(number1, number2);
+    console.log ("RESTA - Resultado: "+result);
+    res.json({
+        resultado: result
+    })
+}
+
+function multiplicar(req, res){
+    const {body} = req;
+    const {number1, number2} = body;
+    const result = multiply(number1, number2);
+    console.log ("MULTIPLICACION - Resultado: "+result);
+    res.json({
+        resultado: result
+    })
+}
+
+function mayor(req, res){
+    const {body} = req;
+    const {number1, number2} = body;
+    const result = numMax(number1, number2);
+    res.json({
+        resultado: result
+    })
+}
+
+module.exports = {
+    sumar,
+    restar,
+    multiplicar,
+    mayor
+}
